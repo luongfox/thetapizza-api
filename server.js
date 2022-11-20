6364ed2e4342472f61ed1da41e23323e7f24eff4
Luong Le <luong@originalstitch.com>
2022-11-20 20:29:08 +0700

    Remote api prefix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api/v1', apiRoutes);
+app.use('/v1', apiRoutes);
 app.use(express.static('public'));
 
 app.listen(PORT, HOST, () => {
